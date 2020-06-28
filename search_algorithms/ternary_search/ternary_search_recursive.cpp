7fd3b007055f5fbb7a09a8c6c068ad4ce5f422d4
Melike VURUCU <melikev63@gmail.com>
2020-06-28 09:55:47 +0300

    added ternary search

---
+++ search_algorithms/ternary_search/ternary_search_recursive.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+// I think code is simple but some people may get confused, thus, I added comments to explain.
+// Hoping it was useful and informative. ^v^
+
+int ternarySearchRecursive (int* arr, int l, int r, int elem) {
+    int lowMid = l + (r - l) / 3; //lower mid is at 1/3 of array
+    int highMid = r - (r - l) / 3; //upper mid is at 2/3 of array
+    if (arr[lowMid] == elem) return lowMid; 
+    if (arr[highMid] == elem) return highMid;
+    else if (arr[lowMid] > elem) return ternarySearchRecursive(arr, l, lowMid - 1, elem); 
+      //set right to 1/3 - 1
+      //because we looked at 1/3 but if it's smaller than 1/3, it has to be at 1st subarray
+    else if (elem > arr[highMid]) return ternarySearchRecursive(arr, highMid + 1, r, elem);
+      //set left to 2/3 + 1
+      //also we looked at 2/3 but if it's greater than 2/3, it has to be at 3rd subarray
+    else return ternarySearchRecursive(arr, lowMid + 1, highMid - 1, elem);
+      //set left to 1/3 + 1
+      //set right to 2/3 - 1
+      //if it's greater than 1/3 but lesser than 2/3, it has to be at 2nd subarray
+  return -1;
+}
+
+int main(void) {
+  int size = 0, element = 0, ind = 0;
+  cout << "Please enter the size of the array to ternary search: ";
+  cin >> size;
+  cout << "Please enter the element to find: ";
+  cin >> element;
+  int arr[size + 1];
+  cout << "Please enter the array: ";
+  for (int i = 0; i < size; ++i) {
+    cin >> arr[i];
+  }
+  ind = ternarySearchRecursive(arr, 0, size - 1, element);
+  cout << element << " is found at index of " << ind << ".\n";
+  cout << "Press any key to continue...";
+  getch();
+  return 0;
+}
