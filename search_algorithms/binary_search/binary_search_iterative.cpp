a0f62ce761c777c6325d4eb8a6a6d0b8d3bfacf5
Melike VURUCU <melikev63@gmail.com>
2020-06-28 13:29:19 +0300

    fixed

--- search_algorithms/binary_search/binary_search_iterative.cpp
+++ search_algorithms/binary_search/binary_search_iterative.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 int binarySearchIterative(int* arr, int l, int r, int element) {
   while (l <= r) { //while array is sizeable (is reasonable)
-    int mid = (r - l) / 2; //divide array to 2 parts 
+    int mid = l + (r - l) / 2; //divide array to 2 parts 
     if (arr[mid] == element) return mid; //if at middle, return middle
     else if (arr[mid] > element) r = mid - 1; //if it's lesser than middle, it has to be at 1st subarray
     else l = mid + 1; //if it's greater, it has to be at 2nd subarray
--- search_algorithms/binary_search/binary_search_recursive.cpp
+++ search_algorithms/binary_search/binary_search_recursive.cpp
@@ -7,7 +7,7 @@ using namespace std;
 // Hoping it was useful and informative. ^v^
 
 int binarySearchRecursive(int* arr, int l, int r, int element) {
-  int mid = (r - l) / 2; //divide array to two parts
+  int mid = l + (r - l) / 2; //divide array to two parts
   if (arr[mid] == element) return mid; //if at middle, return it   
   else if (arr[mid] > element) return binarySearchRecursive(arr, l, mid - 1, element); //if lesser than middle, it has to be at 1st subarray
   else return binarySearchRecursive(arr, mid + 1, r, element); //if greater, has to be at the 2nd
